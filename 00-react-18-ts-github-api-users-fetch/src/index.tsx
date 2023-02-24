e762231d75e19f18282243587d7d2fc3dcaab9dc
danielOuattara <daniel.ouattara@gmx.com>
2023-02-24 22:44:36 +0100

    starting project 00 github users fetching

---
+++ 00-react-18-ts-github-api-users-fetch/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppFunction from "./_01_functions_solution/AppFunction";
+// import AppClass from "./_02_classes_solution/AppClass";
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <AppFunction />
+    <hr />
+    {/* <AppClass /> */}
+  </React.StrictMode>,
+);
