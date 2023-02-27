1719fd4347897d78150c34ff044cf437195a33e9
danielOuattara <daniel.ouattara@gmx.com>
2023-02-27 19:45:18 +0100

    index.tsx

--- 00-react-18-ts-github-api-users-fetch/src/index.tsx
+++ 00-react-18-ts-github-api-users-fetch/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppFunction from "./_01_functions_solution/AppFunction";
-// import AppClass from "./_02_classes_solution/AppClass";
+import AppClass from "./_02_classes_solution/AppClass";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -11,6 +11,6 @@ root.render(
   <React.StrictMode>
     <AppFunction />
     <hr />
-    {/* <AppClass /> */}
+    <AppClass />
   </React.StrictMode>,
 );
