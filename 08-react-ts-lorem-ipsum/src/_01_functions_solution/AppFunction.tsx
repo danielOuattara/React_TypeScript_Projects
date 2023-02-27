dac1b2d328a74fac39b9d0d1ed950f275cb7c6d1
danielOuattara <daniel.ouattara@gmx.com>
2023-02-27 19:49:22 +0100

    function solution OK

---
+++ 08-react-ts-lorem-ipsum/src/_01_functions_solution/AppFunction.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import data from "./../data";
+
+function App() {
+  const [numberOfParagraph, setNumberOfParagraph] = useState("1");
+  const [text, setText] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    return setText(() => data.slice(0, parseInt(numberOfParagraph)));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum ?</h3>
+
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="numbParagraph">number of paragraph ?</label>
+        <input
+          id="numbParagraph"
+          type="number"
+          min="1"
+          max={data.length}
+          value={numberOfParagraph}
+          onChange={(event) => setNumberOfParagraph(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((item: string, index: number) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
+}
+
+export default App;
