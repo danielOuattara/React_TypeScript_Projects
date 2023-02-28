4ce136caea32895db7d309c983c0cd08cc761aeb
danielOuattara <daniel.ouattara@gmx.com>
2023-02-28 22:09:42 +0100

    function solution

---
+++ 09-react-ts-color-generator-app/src/_01_functions_solution/SingleColorFunction.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import Values from "values.js";
+
+type PropsType = {
+  index: number;
+  color: Values;
+  listLength: number;
+};
+
+function SingleColorFunction({ index, listLength, color }: PropsType) {
+  const [alert, setAlert] = useState(false);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => setAlert(false), 500);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
+
+  const handleCopyToClipBoard = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(color.hexString());
+  };
+
+  return (
+    <article
+      onClick={handleCopyToClipBoard}
+      className={index > listLength / 2 + 2 ? "color-light color" : "color"}
+      style={{ backgroundColor: `${color.hexString()}` }}
+    >
+      <p className="percent-value">{color.weight} %</p>
+      <p className="color-value">{color.hexString()}</p>
+      {alert && (
+        <span
+          className={index > listLength / 2 ? "alert-light alert" : "alert"}
+        >
+          copied to clipboard
+        </span>
+      )}
+    </article>
+  );
+}
+
+export default SingleColorFunction;
