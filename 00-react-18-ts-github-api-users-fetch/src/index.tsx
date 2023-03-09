e91c2a4ac83400d7b224cc075768c8424574a9a9
danielOuattara <daniel.ouattara@gmx.com>
2023-03-09 11:15:28 +0100

    index.tsx update

--- 00-react-18-ts-github-api-users-fetch/src/index.tsx
+++ 00-react-18-ts-github-api-users-fetch/src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppFunction from "./_01_functions_solution/AppFunction";
-import AppClass from "./_02_classes_solution/AppClass";
+import AppFunctionV2 from "./_01_functions_solution_v2/AppFunctionV2";
+// import AppClass from "./_02_classes_solution/AppClass";
 import AppClassV2 from "./_02_classes_solution_v2/AppClassV2";
 import "./index.css";
 
@@ -12,8 +13,13 @@ root.render(
   <React.StrictMode>
     <AppFunction />
     <hr />
-    {/* <AppClass /> */}
+    <br />
+    <AppFunctionV2 />
     <hr />
+    <br />
+    {/* <AppClass /> */}
+    {/* <hr /> */}
+    <br />
     <AppClassV2 />
   </React.StrictMode>,
 );
