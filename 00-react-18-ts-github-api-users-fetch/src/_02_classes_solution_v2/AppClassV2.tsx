7623581f74563b1718be740f20ad6115cca89f54
danielOuattara <daniel.ouattara@gmx.com>
2023-03-09 11:15:11 +0100

    update logic for  Class solu. V2

--- 00-react-18-ts-github-api-users-fetch/src/_02_classes_solution_v2/AppClassV2.tsx
+++ 00-react-18-ts-github-api-users-fetch/src/_02_classes_solution_v2/AppClassV2.tsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 import UserType from "./types/UserType";
 import UserListClass from "./components/UsersListClass";
+import SingleUser from "./components/SingleUser";
 
-export type PropsType = {};
 export type StateType = {
   isLoading: boolean;
   isError: boolean;
@@ -10,34 +10,97 @@ export type StateType = {
   users: UserType[];
 };
 
+export type GlobalStateType = {
+  usersState: {
+    isLoading: boolean;
+    isError: boolean;
+    errorMessage: string;
+    users: UserType[];
+  };
+  singleUserState: {
+    singleUserLoading: boolean;
+    singleUserError: boolean;
+    singleUserErrorMessage: string;
+    singleUserData: UserType | {};
+  };
+};
+
 const url = "https://api.github.com/users";
 
 export default class AppClass extends Component {
-  state: StateType = {
-    isLoading: true,
-    isError: false,
-    errorMessage: "",
-    users: [],
+  state: GlobalStateType = {
+    usersState: {
+      isLoading: true,
+      isError: false,
+      errorMessage: "",
+      users: [],
+    },
+    singleUserState: {
+      singleUserLoading: true,
+      singleUserError: false,
+      singleUserErrorMessage: "",
+      singleUserData: {},
+    },
   };
 
+  //------------
   fetchUsers = async () => {
     try {
       const res = await fetch(url);
       if (!res.ok) {
-        this.setState((prevState: StateType) => ({
+        this.setState((prevState: GlobalStateType) => ({
           ...prevState,
-          isError: true,
-          isLoading: false,
-          errorMessage: `${res.statusText} ${res.status}`,
+          usersState: {
+            ...prevState.usersState,
+            isError: true,
+            isLoading: false,
+            errorMessage: `${res.statusText} ${res.status}`,
+          },
         }));
         throw Error(res.statusText);
       } else {
         const fetchedUsers = await res.json();
-        this.setState((prevState: StateType) => ({
+        this.setState((prevState: GlobalStateType) => ({
           ...prevState,
-          isLoading: false,
-          isError: false,
-          users: fetchedUsers,
+          usersState: {
+            ...prevState.usersState,
+            isLoading: false,
+            isError: false,
+            users: fetchedUsers,
+          },
+        }));
+      }
+    } catch (err) {
+      console.log((err as Error).message);
+    }
+  };
+
+  //-------------------
+  fetchSingleUser = async (usernameArg: string) => {
+    try {
+      const res = await fetch(`${url}/${usernameArg}`);
+      if (!res.ok) {
+        this.setState((prevState: GlobalStateType) => ({
+          ...prevState,
+          singleUserState: {
+            ...prevState.singleUserState,
+            singleUserError: true,
+            singleUserLoading: false,
+            singleUserErrorMessage: `${res.statusText} ${res.status}`,
+          },
+        }));
+        throw Error(res.statusText);
+      } else {
+        const fetchedUser = await res.json();
+        this.setState((prevState: GlobalStateType) => ({
+          ...prevState,
+          singleUserState: {
+            ...prevState.singleUserState,
+            singleUserError: false,
+            singleUserLoading: false,
+            singleUserErrorMessage: "",
+            singleUserData: fetchedUser,
+          },
         }));
       }
     } catch (err) {
@@ -52,8 +115,12 @@ export default class AppClass extends Component {
   render() {
     return (
       <>
-        <h2>class component</h2>
-        <UserListClass users={this.state.users} />
+        <h2> react + typescript class solution</h2>
+        <SingleUser
+          singleUserState={this.state.singleUserState}
+          fetchSingleUser={this.fetchSingleUser}
+        />
+        <UserListClass users={this.state.usersState.users} />
       </>
     );
   }
