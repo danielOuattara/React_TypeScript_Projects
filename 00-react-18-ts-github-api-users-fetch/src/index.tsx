d249d2da037113c3a248a845467da25944d5ff98
danielOuattara <daniel.ouattara@gmx.com>
2023-03-09 08:56:59 +0100

    correcting AppClassV2 calling

--- 00-react-18-ts-github-api-users-fetch/src/index.tsx
+++ 00-react-18-ts-github-api-users-fetch/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppFunction from "./_01_functions_solution/AppFunction";
 import AppClass from "./_02_classes_solution/AppClass";
+import AppClassV2 from "./_02_classes_solution_v2/AppClassV2";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -11,6 +12,8 @@ root.render(
   <React.StrictMode>
     <AppFunction />
     <hr />
-    <AppClass />
+    {/* <AppClass /> */}
+    <hr />
+    <AppClassV2 />
   </React.StrictMode>,
 );
