9d8b3202156ead05b0bb0f87ab74209aa0cabe50
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 15:06:34 +0100

    11 navbar: completing functions solution

---
+++ 11-react-ts-navbar-app/src/_01_functions_solution/AppFunction.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+export default function AppFunction() {
+  return (
+    <>
+      <b>
+        <p style={{ marginLeft: "5rem" }}>function typescript solution</p>
+      </b>
+      <Navbar />
+    </>
+  );
+}
