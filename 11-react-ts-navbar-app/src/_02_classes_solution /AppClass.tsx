294c74758d2c36c1ea52e4b266fd7ee2e3362d2e
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 15:10:32 +0100

    11 navbar class solution

---
+++ 11-react-ts-navbar-app/src/_02_classes_solution /AppClass.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Navbar from "./components/Navbar";
+
+export default class AppClass extends Component {
+  render() {
+    return (
+      <>
+        <b>
+          <p style={{ marginLeft: "5rem" }}>class typescript solution</p>
+        </b>
+        <Navbar />
+      </>
+    );
+  }
+}
