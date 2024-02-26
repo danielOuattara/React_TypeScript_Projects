d6330fd39e6fa2fe8ab7113c095fd3f16d3f99c9
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-26 13:53:42 +0100

    root files

--- 01-react-18-ts-birthday-reminder/src/index.tsx
+++ 01-react-18-ts-birthday-reminder/src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppFunction from "./_01_functions_solution/AppFunction";
 import AppClass from "./_02_classes_solution/AppClass";
+import AppContextAPI from "./_03_contextAPI_functions_solution/AppFunctionContextAPI";
+import AppClassContextAPI from "./_04_contextAPI_classes_solution/AppClassContextAPI";
+import AppFunctionContextAPIUseContextHook from "./_05_contextAPI_useContext_solution/AppFunctionContextAPIUseContextHook";
+import AppFunctionUserReducer from "./_06_useReducer_hook_functions_solution/AppFunctionUseReducer";
+import AppFunctionUseReducerContextAPI from "./_07_useReducer_context_API_functions_solution/AppFunctionUseReducerContextAPI";
+import AppFunctionUseReducerUseContext from "./_08_useReducer_useContext_hooks_functions_solution/AppFunctionUseReduceruseContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -12,5 +18,17 @@ root.render(
     <AppFunction />
     <hr />
     <AppClass />
+    <hr />
+    <AppContextAPI />
+    <hr />
+    <AppClassContextAPI />
+    <hr />
+    <AppFunctionContextAPIUseContextHook />
+    <hr />
+    <AppFunctionUserReducer />
+    <hr />
+    <AppFunctionUseReducerContextAPI />
+    <hr />
+    <AppFunctionUseReducerUseContext />
   </React.StrictMode>,
 );
