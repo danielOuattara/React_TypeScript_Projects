55fa691b21714be3e2bc3bd4ae881eb838594dfb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-07 14:58:07 +0100

    02: travel tour:  04 contextAPI classes solution v1

--- 02-react-ts-tours-travel-app/src/_04_contextAPI_classes_solution/AppClassContextAPI.tsx
+++ 02-react-ts-tours-travel-app/src/_04_contextAPI_classes_solution/AppClassContextAPI.tsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import ToursContextProvider from "./context/ToursContext";
 import Container from "./components/Container";
 
-export default class AppClass extends Component {
+export default class AppClassContextAPI extends Component {
   render() {
     return (
       <ToursContextProvider>
