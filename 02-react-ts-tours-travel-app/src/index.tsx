cecfc4b2687a94b260d7a3407b9393371b08dd3b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-07 14:58:41 +0100

    02: travel tour:  src & packages

--- 02-react-ts-tours-travel-app/src/index.tsx
+++ 02-react-ts-tours-travel-app/src/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import AppFunction from "./_01_function_solution/AppFunction";
-import AppClass from "./_02_classes_solution/AppClass";
+// import AppFunction from "./_01_function_solution_v1/AppFunction";
+// import AppFunctionV2 from "./_01_function_solution_v2/AppFunctionV2";
+// import AppClass from "./_02_classes_solution/AppClass";
+// import AppFunctionContextAPI from "./_03_contextAPI_functions_solution_v1/AppFunctionContextAPIV1";
+// import AppFunctionContextAPIV2 from "./_03_contextAPI_functions_solution_v2/AppFunctionContextAPIV2";
+// import AppClassContextAPI from "./_04_contextAPI_classes_solution/AppClassContextAPI";
+import AppClassContextAPIV2 from "./_04_contextAPI_classes_solution v2/AppClassContextAPIV2";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -11,6 +16,16 @@ root.render(
   <React.StrictMode>
     {/* <AppFunction /> */}
     <hr />
-    <AppClass />
+    {/* <AppFunctionV2 /> */}
+    <hr />
+    {/* <AppClass /> */}
+    <hr />
+    {/* <AppFunctionContextAPI /> */}
+    <hr />
+    {/* <AppFunctionContextAPIV2 /> */}
+    <hr />
+    {/* <AppClassContextAPI /> */}
+    <hr />
+    <AppClassContextAPIV2 />
   </React.StrictMode>,
 );
